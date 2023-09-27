bb76a89df96d4cf9029eea56c1064646ab03d223
davigimenez_cs@hotmail.com <davidgimenez_cs@hotmail.com>
2023-09-27 13:25:25 +0200

    Modificación del ejercicio por error y mejorar la visualización de la escritura de los números

--- TEMA1/Ejercicio7.java
+++ TEMA1/Ejercicio7.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
+        System.out.println("Escriba el primer número: ");
         int a= in.nextInt();
+        System.out.println("Escriba el segundo número: ");
         int b= in.nextInt();
         if (a>b){
             System.out.println(a-b);
@@ -10,7 +12,7 @@ public class Ejercicio7 {
         if (b>a){
             System.out.println(b-a);
         }
-        else {
+        if (a==b) {
             System.out.println("0");
         }
     }
