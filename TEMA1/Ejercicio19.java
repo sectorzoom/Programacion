555b3a7366781bd0ae38c5be915378cbf82d4ec5
davigimenez_cs@hotmail.com <davidgimenez_cs@hotmail.com>
2023-09-28 09:14:51 +0200

    Ejercicios de programación estructurada: Estructuras Condicionales Ejercicio 19

---
+++ TEMA1/Ejercicio19.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        for (int i = 2; i < 1000; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
