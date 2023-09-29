fe617891aa3371aac6e8e8b6834e20a16bbb6e12
davigimenez_cs@hotmail.com <davidgimenez_cs@hotmail.com>
2023-09-29 22:28:11 +0200

    Lo he revisado y mejorado reduciendo código inservible.

--- TEMA1/Ejercicio26.java
+++ TEMA1/Ejercicio26.java
@@ -5,23 +5,17 @@ public class Ejercicio26 {
         Scanner in = new Scanner(System.in);
         System.out.println("Escriba su supuesto numero primo: ");
         int numero = in.nextInt();
-        if (numero<=1){
-            System.out.println("El número no es primo.");
-        } else if (numero==2) {
+        boolean esprimo = true;
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                esprimo = false;
+                break;
+            }
+        }
+        if (esprimo) {
             System.out.println("El número es primo.");
-        } else {
-            boolean esprimo = true;
-            for (int i = 2; i < numero; i++) {
-                if (numero % i == 0) {
-                    esprimo = false;
-                    break;
-                }
-            }
-            if (esprimo) {
-                System.out.println("El número es primo.");
-            } else {
-                System.out.println("El número no es primo.");
-            }
+        }else {
+            System.out.println("El número no es primo.");
         }
     }
 }
