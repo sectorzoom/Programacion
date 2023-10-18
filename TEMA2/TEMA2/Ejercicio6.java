f09632c00d14ee47a4dc5b9a0cc59678a9b09b70
davigimenez_cs@hotmail.com <davidgimenez_cs@hotmail.com>
2023-10-18 12:47:21 +0200

    Actividades Programación Modular Actividad6

---
+++ TEMA2/TEMA2/Ejercicio6.java
@@ -0,0 +1,12 @@
+package TEMA2;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        System.out.println("Todas las tablas de multiplicar del 1 al 10");
+
+        for (int number = 1; number <= 10; number++) {
+            Ejercicio5.multiplication_table(number);
+        }
+
+    }
+}
