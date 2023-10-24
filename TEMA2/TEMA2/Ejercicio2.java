cc57780cc32906b076f44639d02c8a5153e87cc2
davigimenez_cs@hotmail.com <davidgimenez_cs@hotmail.com>
2023-10-24 17:44:50 +0200

    Actividades Programación Modular Actividad2

--- TEMA2/TEMA2/Ejercicio2.java
+++ TEMA2/TEMA2/Ejercicio2.java
@@ -7,15 +7,13 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Introduce la edad a comprobar: ");
-        int a;
-        a = in.nextInt();
-        if (isAdult(a)) {
+        int age;
+        age = in.nextInt();
+        if (isAdult(age)) {
             System.out.println("Es mayor de edad.");
-        }
-        if (Ejercicio1.numberSign(a)<0) {
+        } else if (age < 0) {
             System.out.println("Edad incorrecta.");
-        }
-        else {
+        } else {
             System.out.println("No es mayor de edad.");
         }
 
