2301129a7a2484d840e797502fae6b5584552340
davigimenez_cs@hotmail.com <davidgimenez_cs@hotmail.com>
2023-10-25 00:11:08 +0200

    Actividades Programación Modular Actividad5

--- TEMA2/TEMA2/Ejercicio5.java
+++ TEMA2/TEMA2/Ejercicio5.java
@@ -1,41 +1,43 @@
 package TEMA2;
 import java.util.Scanner;
 public class Ejercicio5 {
-    public static void multiplication_table (int number) {
+    public static void multiplicationTable(int number) {
         System.out.println("La tabla de multiplicar de " + number + " es:");
         int multiplicator = 0;
         do {
             System.out.println(multiplicator + " x " + number + " = " + (multiplicator * number));
             multiplicator++;
-        }while (multiplicator<11);
+        } while (multiplicator < 11);
+    }
+    public static boolean selectionNumber(int selection) {
+        return selection <= 0 || selection > 10;
+    }
+    public static boolean repeatTable(char repeat) {
+        boolean correct = repeat == 's' || repeat == 'S';
+        if (!correct) {
+            System.out.println("Introduzca un valor válido. S o N");
+        }
+        return correct;
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         boolean select = true;
-        while (select){
-            int number = 0;
-            while ((number <= 0) || (number > 10)) {
-                System.out.println("Por favor, introduzca el número que quiere saber su tabla:");
+        while (select) {
+            System.out.println("Introduzca el número del que quiere saber su tabla");
+            int number = in.nextInt();
+            while (selectionNumber(number)) {
+                System.out.println("Por favor, introduzca un valor válido, entre 0 y 10");
                 number = in.nextInt();
-                if ((number <= 0) || (number > 10)) {
-                    System.out.println("Introduzca un número del 1 al 10");
-                }
             }
-            multiplication_table(number);
+            multiplicationTable(number);
             System.out.println("¿Quieres ver la tabla de otro número?");
-            char selection;
             while (true) {
-                selection = in.next().charAt(0);
-                if ((selection == 'n') || (selection == 'N')) {
+                char selection = in.next().charAt(0);
+                if (selection == 'n' || selection == 'N') {
                     select = false;
                     break;
-                }
-                if ((selection == 's') || (selection == 'S')) {
+                } else if (repeatTable(selection))
                     break;
-                }
-                else {
-                    System.out.println("Por favor, escriba s o n");
-                }
             }
         }
     }
