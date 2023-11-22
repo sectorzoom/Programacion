63d69a700f6d9b79b0e1920cdf86586518536038
sectorzoom <sectorzoom@gmail.com>
2023-11-22 14:06:23 +0100

    a

--- TEMA3/ActividadesArrays.java
+++ TEMA3/ActividadesArrays.java
@@ -3,54 +3,125 @@ import java.util.Scanner;
 public class ActividadesArrays {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        exercise1();
+        selection();
     }
-    public static void exercise1 () {
-        int[] example = new int[5];
-        int[] example2 = new int[5];
-        System.out.println("a) Procedimiento que imprima el array.");
-        randomNumbers(example);
-        printArray(example);
+    public static void exercise1 (int[] array1, int[] array2) {
+        boolean option = true;
+        while (option){
+            System.out.println("""
+                Introduzca qué ejercicio quiere hacer:
+                a) Procedimiento que imprima el array.
+                b) Función que devuelva el máximo del array.
+                c) Función que devuelva el mínimo del array.
+                d) Función que devuelva la media
+                e) Función que te diga si un elemento existe en el array o no
+                f) Función que haga la suma de dos vectores (arrays)
+                g) Función que haga la resta de dos vectores (arrays)
+                h) Función que haga el producto escalar de dos vectores (arrays)
+                i) Función que invierta el orden de un array
+                j) Procedimiento que invierta el orden de un array
+                k) Función que devuelva si un array es capicua""");
+            System.out.print("Indique aquí su elección:");
+            char election = in.next().charAt(0);
+            switch (election) {
+                case 'a':
+                    System.out.println("a) Procedimiento que imprima el array.");
+                    printArray(array1);
+                    printArray(array2);
+                    break;
+                case 'b':
+                    System.out.println("b) Función que devuelva el máximo del array.");
+                    System.out.println(maxValue(array1));
+                    System.out.println(maxValue(array2));
+                    break;
+                case 'c':
+                    System.out.println("c) Función que devuelva el mínimo del array.");
+                    System.out.println(minValue(array1));
+                    System.out.println(minValue(array2));
+                    break;
+                case 'd':
+                    System.out.println("d) Función que devuelva la media");
+                    System.out.println(average(array1));
+                    System.out.println(average(array2));
+                    break;
+                case 'e':
+                    System.out.println("e) Función que te diga si un elemento existe en el array o no");
+                    if (ifExistElement(array1)) {
+                        System.out.println("Existe");
+                    } else {
+                        System.out.println("No Existe");
+                    }
+                    if (ifExistElement(array2)) {
+                        System.out.println("Existe");
+                    } else {
+                        System.out.println("No Existe");
+                    }
+                    break;
+                case 'f':
+                    System.out.println("f) Función que haga la suma de dos vectores (arrays)");
+                    printArray(array1);
+                    System.out.print(" + ");
+                    printArray(array2);
+                    System.out.print(" = ");
+                    System.out.println(Arrays.toString(additionArrays(array1, array2)));
+                    break;
+                case 'g':
+                    System.out.println("g) Función que haga la resta de dos vectores (arrays)");
+                    printArray(array1);
+                    System.out.print(" - ");
+                    printArray(array2);
+                    System.out.print(" = ");
+                    System.out.println(Arrays.toString(minusArrays(array1, array2)));
+                    break;
+                case 'h':
+                    System.out.println("h) Función que haga el producto escalar de dos vectores (arrays)");
+                    printArray(array1);
+                    System.out.print(" * ");
+                    printArray(array2);
+                    System.out.print(" = ");
+                    System.out.println(scalarProduct(array1, array2));
+                    break;
+                case 'i':
+                    System.out.println("i) Función que invierta el orden de un array");
+                    System.out.println(Arrays.toString(invertArrayFunction(array1)));
+                    System.out.println(Arrays.toString(invertArrayFunction(array2)));
+                    break;
+                case 'j':
+                    System.out.println("j) Procedimiento que invierta el orden de un array");
+                    invertArrayVoid(array1);
+                    invertArrayVoid(array2);
+                    break;
+                case 'k':
+                    System.out.println("k) Función que devuelva si un array es capicua");
+                    if (capicua(array1)) {
+                        System.out.println("Es capicua");
+                    } else {
+                        System.out.println("No es capicua");
+                    }
+                    if (capicua(array2)) {
+                        System.out.println("Es capicua");
+                    } else {
+                        System.out.println("No es capicua");
+                    }
+                    break;
+                case 's':
+                    option = false;
+                    break;
+                default:
+                    System.out.println("Introduce un valor correcto");
+            }
 
-        System.out.println("b) Función que devuelva el máximo del array.");
-        System.out.println(maxValue(example));
-
-        System.out.println("c) Función que devuelva el mínimo del array.");
-        System.out.println(minValue(example));
-
-        System.out.println("d) Función que devuelva la media");
-        System.out.println(average(example));
-
-        System.out.println("e) Función que te diga si un elemento existe en el array o no");
-        if (ifExistElement(example)) {
-            System.out.println("Existe");
-        } else {
-            System.out.println("No Existe");
         }
 
-        System.out.println("f) Función que haga la suma de dos vectores (arrays)");
-        printArray(example);
-        System.out.println("         +");
-        randomNumbers(example2);
-        printArray(example2);
-        System.out.println("         =");
-        System.out.println(Arrays.toString(additionArrays(example, example2)));
 
-        System.out.println("g) Función que haga la resta de dos vectores (arrays)");
-        printArray(example);
-        System.out.println("         -");
-        randomNumbers(example2);
-        printArray(example2);
-        System.out.println("         =");
-        System.out.println(Arrays.toString(substractionArrays(example, example2)));
 
-        System.out.println("h) Función que haga el producto escalar de dos vectores (arrays)");
-        printArray(example);
-        System.out.println("         *");
-        randomNumbers(example2);
-        printArray(example2);
-        System.out.println("         =");
-        System.out.println(scalarProduct(example, example2));
+
+
+
+
+
+
+
     }
     public static void randomNumbers (int[] random) {
         for (int i = 0; i < random.length; i++) {
@@ -125,7 +196,7 @@ public class ActividadesArrays {
         }
         return result;
     }
-    public static int[] substractionArrays (int[] array1, int[] array2) {
+    public static int[] minusArrays(int[] array1, int[] array2) {
         int[] result;
         if (array1.length != array2.length) {
             if (array1.length > array2.length) {
@@ -154,10 +225,29 @@ public class ActividadesArrays {
     }
     public static int scalarProduct (int[] array1, int[] array2) {
         int length = Math.max(array1.length, array2.length);
-        int[] result = new int[length];
+        int[] result;
         int suma = 0;
-        for (int i = 0; i < length; i++) {
-            result[i] = array1[i] * array2[i];
+        if (array1.length != array2.length) {
+            if (array1.length > array2.length) {
+                result = new int[array1.length];
+                for (int i = 0; i < array2.length; i++) {
+                    result[i] = array1[i] * array2[i];
+                }
+                System.arraycopy(array1, array2.length, result, array2.length, array1.length - array2.length);
+            }
+            else {
+                result = new int[array2.length];
+                for (int i = 0; i < array1.length; i++) {
+                    result[i] = array2[i] * array1[i];
+                }
+                System.arraycopy(array2, array1.length, result, array1.length, array2.length - array1.length);
+            }
+
+        } else {
+            result = new int[length];
+            for (int i = 0; i < length; i++) {
+                result[i] = array2[i] * array1[i];
+            }
         }
         for (int i : result) {
             suma += i;
@@ -165,12 +255,68 @@ public class ActividadesArrays {
         }
         return suma;
     }
-    public static int[] invertArray (int[]array) {
+    public static int[] invertArrayFunction (int[]array) {
+
+        System.out.println("La array inversa de " + Arrays.toString(array) + " es: ");
         int length = array.length-1;
         int[] array2 = array.clone();
         for(int i = 0; i < array.length; i++) {
             array[i] = array2[length-i];
         }
         return array;
+    }
+    public static void invertArrayVoid (int[]array) {
+        System.out.println("La array inversa de " + Arrays.toString(array) + " es: ");
+        int length = array.length-1;
+        int[] array2 = array.clone();
+        for(int i = 0; i < array.length; i++) {
+            array[i] = array2[length-i];
+        }
+        System.out.println(Arrays.toString(array));
+    }
+    public static boolean capicua (int[]array) {
+        boolean capicua = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                capicua = false;
+                break;
+            }
+        }
+        return capicua;
+    }
+    public static void selection () {
+        System.out.println("Qué te gustaría hacer, los ejercicios introduciendo tus propias arrays o con arrays aleatorias?");
+        System.out.println("1) Aleatorio\n2) Personalizado");
+        int selection = in.nextInt();
+        int[] array1;
+        int[] array2;
+        switch (selection) {
+            case 1:
+                array1 = new int[randomLengthArray()];
+                array2 = new int[randomLengthArray()];
+                randomNumbers(array1);
+                randomNumbers(array2);
+                exercise1(array1,array2);
+                break;
+            case 2:
+                System.out.println("Introduzca el tamaño del array1:");
+                array1 = new int[in.nextInt()];
+                System.out.println("Introduzca el tamaño del array2:");
+                array2 = new int[in.nextInt()];
+                System.out.println("Introduzca los números de tu array1:");
+                personalizeArray(array1);
+                System.out.println("Introduzca los números de tu array2:");
+                personalizeArray(array2);
+                exercise1(array1,array2);
+        }
+    }
+    public static int randomLengthArray () {
+        return (int) (Math.random() * 10);
+    }
+    public static void personalizeArray (int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Introduzca el numero en la posición " + i+1 + " de " + array.length);
+            array[i] = in.nextInt();
+        }
     }
 }
