38291d1b6d363cd3065394438f625d4e6af03fb4
sectorzoom <davidgimenez_cs@hotmail.com>
2023-11-23 17:13:43 +0100

    Ejercicio 4 terminado

--- TEMA3/Actividad4.java
+++ TEMA3/Actividad4.java
@@ -4,19 +4,28 @@ import java.util.Scanner;
 public class Actividad4 {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Vamos a simular la cobertura que habrá según la potencia y lugar de colocación del router.\n" +
-                "Primero, introduzca en qué habitación estará el router\n(Elija del 1 al 20)");
+        System.out.println("""
+                Vamos a simular la cobertura que habrá según la potencia y lugar de colocación del router.
+                Primero, introduzca en qué habitación estará el router
+                (Elija del 1 al 20)""");
         int[] hotelsRoom = new int[20];
         int selectionRoom = in.nextInt();
-        System.out.println("Ahora, introduzca la potencia del router\n(Elija una potencia de señal de 1 a 6.");
+        System.out.println("Ahora, introduzca la potencia del router\n(Elija una potencia de señal de 1 a 6).");
         int routerPower = in.nextInt();
         System.out.println("La simulación de la cobertura es la siguiente");
-        for (int i = 0; i < hotelsRoom.length; i++) {
-            if (i == selectionRoom) {
-                hotelsRoom[i] = routerPower - i;
+        int room = selectionRoom-1;
+        for (int i = routerPower; i != 0; room--){
+            hotelsRoom[room] = i;
+            i--;
+        }
+        room = selectionRoom-1;
+        for (int i = routerPower; room != 20; room++){
+            hotelsRoom[room] = i;
+            i--;
+            if (i < 0) {
+                hotelsRoom[room] = 0;
             }
         }
         System.out.println(Arrays.toString(hotelsRoom));
-
     }
 }
