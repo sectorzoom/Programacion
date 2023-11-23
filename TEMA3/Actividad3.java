c49f5d8257a37e77f82f4d781e03e85bbe4af7db
sectorzoom <davidgimenez_cs@hotmail.com>
2023-11-23 18:01:43 +0100

    Ejercicio 3 cambio nombre de variables

--- TEMA3/Actividad3.java
+++ TEMA3/Actividad3.java
@@ -16,26 +16,26 @@ public class Actividad3 {
     }
     public static void game () {
         int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        int fly = whereIsFly(array);
+        int fly = flyFly(array);
         boolean winner = false;
         int round = 1;
         while (!winner) {
             System.out.println("Ronda " + round + ": ");
             round++;
             int selection = in.nextInt();
-            fly = changePositionFly(selection, array, fly);
+            fly = whereIsFly(selection, array, fly);
             if (fly == selection) {
                 winner = true;
             }
         }
     }
-    public static int whereIsFly(int[]array) {
+    public static int flyFly(int[]array) {
         return array[random.nextInt(14)];
     }
-    public static int changePositionFly (int selection, int[]array, int fly) {
+    public static int whereIsFly(int selection, int[]array, int fly) {
         if (selection == (fly-1) || selection == (fly+1)) {
             System.out.println("Casi pero no!");
-            fly = whereIsFly(array);
+            fly = flyFly(array);
         } else if (selection != fly) {
             System.out.println("Estuviste muy lejos!");
         } else {
