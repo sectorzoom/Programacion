029f2f7adc43d2c9c313946a39674bb3c62c92d3
sectorzoom <sectorzoom@gmail.com>
2023-11-28 10:25:06 +0100

    Solución bug al poner valores en la primera habitación

--- TEMA3/Actividad4.java
+++ TEMA3/Actividad4.java
@@ -9,13 +9,29 @@ public class Actividad4 {
                 Primero, introduzca en qué habitación estará el router
                 (Elija del 1 al 20)""");
         int[] hotelsRoom = new int[20];
-        int selectionRoom = in.nextInt();
+        int selectionRoom = 0;
+        while (selectionRoom < 1){
+            selectionRoom = in.nextInt();
+            if (selectionRoom < 1){
+                System.out.println("Por favor, introduzca un valor entre 1 y 20");
+            }
+        }
         System.out.println("Ahora, introduzca la potencia del router\n(Elija una potencia de señal de 1 a 6).");
-        int routerPower = in.nextInt();
+        int routerPower = 0;
+        while (routerPower < 1){
+            routerPower = in.nextInt();
+            if (routerPower < 1){
+                System.out.println("Por favor, introduzca un valor entre 1 y 6");
+            }
+        }
         System.out.println("La simulación de la cobertura es la siguiente");
         int room = selectionRoom-1;
         for (int i = routerPower; i != 0; room--){
-            hotelsRoom[room] = i;
+            if (room >= 0) {
+                hotelsRoom[room] = i;
+            } else {
+                break;
+            }
             i--;
         }
         room = selectionRoom-1;
