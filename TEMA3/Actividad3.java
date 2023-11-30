d704eca555ee5d3822071a035c7df9a7298f3a5e
sectorzoom <sectorzoom@gmail.com>
2023-11-30 08:51:45 +0100

    Cambio completo, utilización de las arrays

--- TEMA3/Actividad3.java
+++ TEMA3/Actividad3.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,32 +16,30 @@ public class Actividad3 {
         game();
     }
     public static void game () {
-        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        int fly = flyFly(array);
-        boolean winner = false;
+        boolean[] board = new boolean[15];
+        Arrays.fill(board,false);
+        int flyPosition = random.nextInt(14);
+        board[flyPosition] = true;
+        System.out.println(Arrays.toString(board));
         int round = 1;
-        while (!winner) {
+        boolean winner = true;
+        while (winner) {
             System.out.println("Ronda " + round + ": ");
             round++;
-            int selection = in.nextInt();
-            fly = whereIsFly(selection, array, fly);
-            if (fly == selection) {
-                winner = true;
+            int selection = in.nextInt()-1;
+            if (selection == (flyPosition - 1)) {
+                System.out.println("Casi pero no!");
+                board[flyPosition] = false;
+                flyPosition = random.nextInt(14);
+                board[flyPosition] = true;
+                System.out.println(Arrays.toString(board));
+            } else if (board[selection] == board[flyPosition]) {
+                System.out.println("Has cazado a la mosca");
+                winner = false;
+            } else {
+                System.out.println("Estuviste muy lejos!");
             }
         }
-    }
-    public static int flyFly(int[]array) {
-        return array[random.nextInt(14)];
-    }
-    public static int whereIsFly(int selection, int[]array, int fly) {
-        if (selection == (fly-1) || selection == (fly+1)) {
-            System.out.println("Casi pero no!");
-            fly = flyFly(array);
-        } else if (selection != fly) {
-            System.out.println("Estuviste muy lejos!");
-        } else {
-            System.out.println("Has cazado a la mosca!");
-        }
-        return fly;
+
     }
 }
