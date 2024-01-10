9d8e9e0a1aa4f839c518ca7da8f8dc93d25e16a2
sectorzoom <davidgimenez_cs@hotmail.com>
2024-01-10 20:17:13 +0100

    ejercicios clase String a y b

--- TEMA3/Actividades_String/MyString.java
+++ TEMA3/Actividades_String/MyString.java
@@ -5,16 +5,32 @@ import java.util.Scanner;
 public class MyString {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println(invertedString());
+        invertedString();
+        numOfVowels();
     }
-    public static String invertedString () {
+    public static void invertedString () {
         System.out.println("Introduzca la cadena que quiere dar la vuelta y ponerla en mayúscula");
         String string = scanner.next();
-        return invertAndUpperCase(string);
+        System.out.println(string + " al revés y en mayúscula es " + invertAndUpperCase(string));
     }
     public static String invertAndUpperCase(String string){
-        String invertedString = new StringBuilder(string).reverse().toString();
-        return invertedString.toUpperCase();
+        return new StringBuilder(string).reverse().toString().toUpperCase();
+    }
+    public static void numOfVowels(){
+        System.out.println("Introduzca la cadena de la que quiere saber el número de vocales que contiene");
+        String string = scanner.next();
+        System.out.println(string + " tiene " + countNumOfVowels(string) + " vocales.");
+    }
+    public static int countNumOfVowels(String string){
+        String stringLowerCase = string.toLowerCase();
+        char[]characters = stringLowerCase.toCharArray();
+        int count = 0;
+        for (char character : characters) {
+            if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
