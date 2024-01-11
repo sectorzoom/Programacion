4b611bf4c57a1afcc7ba354c8def7e5d1f8802e2
sectorzoom <sectorzoom@gmail.com>
2024-01-11 09:27:23 +0100

    Strings ejercicio c

--- TEMA3/Actividades_String/MyString.java
+++ TEMA3/Actividades_String/MyString.java
@@ -6,19 +6,20 @@ public class MyString {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         invertedString();
-        numOfVowels();
+        sayNumOfVowels();
+        sayTheMostLarge();
     }
     public static void invertedString () {
         System.out.println("Introduzca la cadena que quiere dar la vuelta y ponerla en mayúscula");
-        String string = scanner.next();
+        String string = scanner.nextLine();
         System.out.println(string + " al revés y en mayúscula es " + invertAndUpperCase(string));
     }
     public static String invertAndUpperCase(String string){
         return new StringBuilder(string).reverse().toString().toUpperCase();
     }
-    public static void numOfVowels(){
+    public static void sayNumOfVowels(){
         System.out.println("Introduzca la cadena de la que quiere saber el número de vocales que contiene");
-        String string = scanner.next();
+        String string = scanner.nextLine();
         System.out.println(string + " tiene " + countNumOfVowels(string) + " vocales.");
     }
     public static int countNumOfVowels(String string){
@@ -32,6 +33,24 @@ public class MyString {
         }
         return count;
     }
+    public static void sayTheMostLarge() {
+        System.out.println("Introduzca la cadena que quiere comparar y diré cuál es la palabra más larga que tiene");
+        String string = scanner.nextLine();
+        System.out.println("La palabra más larga es " + theMostLarge(string));
+
+    }
+    public static String theMostLarge(String string) {
+        String[] words = string.split(" ");
+        String mostLargeWord = "";
+        for (String word : words) {
+            if (word.length() > mostLargeWord.length()) {
+                mostLargeWord = word;
+            }
+        }
+        return mostLargeWord;
+    }
+
+
 }
 
 
