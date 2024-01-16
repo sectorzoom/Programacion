a8b2fe5c8a78dbb3ea3b4f760231ae329fb1785c
sectorzoom <sectorzoom@gmail.com>
2024-01-16 10:54:10 +0100

    POO1 reloj falta terminar modo 24h

---
+++ TEMA4/POO1/Reloj.java
@@ -0,0 +1,87 @@
+package POO1;
+public class Reloj {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private int mode;
+    public static final int DEFAULT = 0;
+    public static final int DEFAULTMODE = 12;
+
+    public Reloj(){
+        this.hours = DEFAULT;
+        this.minutes = DEFAULT;
+        this.seconds = DEFAULT;
+        this.mode = DEFAULTMODE;
+
+    }
+    public Reloj(int hours, int minutes, int seconds){
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours, int mode) {
+        if (mode == 12){
+            while (hours > 12){
+                hours = hours - 12;
+            }
+            if (hours < 0){
+                this.hours = DEFAULT;
+            } else{
+                this.hours = hours;
+            }
+        }
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        while (minutes > 59) {
+            minutes = minutes - 60;
+            hours = hours + 1;
+            setHours(hours,mode);
+        }
+        if (minutes < 0){
+            this.minutes = DEFAULT;
+        } else {
+            this.minutes = minutes;
+        }
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        while (seconds > 59) {
+            seconds = seconds - 60;
+            minutes = minutes + 1;
+        }
+        if (seconds < 0){
+            this.seconds = DEFAULT;
+        } else {
+            this.seconds = seconds;
+        }
+    }
+    public void showReloj(){
+        System.out.println("En modo " + getMode() + " la hora es: " + getHours() + ":"+ getMinutes()+ ":" + getSeconds());
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public void setMode(int mode) {
+        if (mode != 12 && mode != 24){
+            this.mode = DEFAULTMODE;
+        } else {
+            this.mode = mode;
+        }
+    }
+}
---
+++ TEMA4/POO1/mainReloj.java
@@ -0,0 +1,24 @@
+package POO1;
+
+import java.util.Scanner;
+
+public class mainReloj {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Reloj reloj = new Reloj();
+        System.out.println("Introduce el modo de hora (12 o 24)");
+        int mode = scanner.nextInt();
+        reloj.setMode(mode);
+        System.out.println("Introduce hora");
+        int hour = scanner.nextInt();
+        System.out.println("Introduce minutos");
+        int minutes = scanner.nextInt();
+        System.out.println("Introduce segundos");
+        int seconds = scanner.nextInt();
+        reloj.setHours(hour,mode);
+        reloj.setMinutes(minutes);
+        reloj.setSeconds(seconds);
+        reloj.showReloj();
+
+    }
+}
