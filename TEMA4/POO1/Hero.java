0458babbddb726cb4af7023389640ba9fbb02750
sectorzoom <davidgimenez_cs@hotmail.com>
2024-01-22 16:59:55 +0100

    Hero ejercicio 3

--- TEMA4/POO1/Hero.java
+++ TEMA4/POO1/Hero.java
@@ -1,17 +1,27 @@
 package POO1;
 
 public class Hero {
+    private final String name;
     private int lvl;
     private int health;
     private int maxHealth;
     private int experience;
     private int attack;
     private int defense;
+    private static final int maxPotion = 10;
     private static final int maxRest = 50;
-    private static final int maxHealthPot = 10;
-    
+    private static final int minExp = 10;
+    private static final int maxExp = 50;
+    private static final int maxLvlUpHealth = 5;
+    private static final int reset = 0;
+
+
+
+
+
 
     public Hero(String name, int lvl, int health, int maxHealth, int experience, int attack, int defense) {
+        this.name = name;
         this.lvl = lvl;
         this.health = health;
         this.maxHealth = maxHealth;
@@ -19,33 +29,42 @@ public class Hero {
         this.attack = attack;
         this.defense = defense;
     }
-    public int drinkPotion(){
-        if (maxHealth-health < maxHealthPot){
-            health = (maxHealth-health) + health;
-        } else {
-            health = health + maxHealthPot;
-        }
+    public int getHealth() {
         return health;
     }
-    public int rest(){
-        if (maxHealth-health < maxRest){
-            health = (maxHealth-health) + health;
-        } else {
-            health = health + maxRest;
-        }
-        return health;
+    public String getName(){
+        return name;
     }
-    public int attack(Hero otherHero){
-        return otherHero.health - Math.max (this.attack - otherHero.defense,10);
+    public void drinkPotion(){
+        health = Math.min(health + maxPotion, maxHealth);
     }
-    public void lvlUp(){
-        experience = experience+10;
-        if (experience == 50){
-            lvl = lvl + 1;
-            maxHealth = maxHealth+5;
-            health = health+5;
-            attack = attack+1;
-            defense = defense+1;
+    public void rest(){
+        health = Math.min(health + maxRest, maxHealth);
+    }
+    public void attack(Hero otherHero) {
+        int damage = Math.max(this.attack - otherHero.defense, 10);
+        otherHero.health = Math.max(0, otherHero.health - damage);
+        experience += minExp;
+        lvlUp();
+    }
+
+    public void lvlUp() {
+        if (experience >= maxExp) {
+            lvl++;
+            maxHealth += maxLvlUpHealth;
+            health += maxLvlUpHealth;
+            attack++;
+            defense++;
+            experience = reset;
         }
+    }
+    public String toString() {
+        return "Hero " +
+                "lvl=" + lvl +
+                ", health=" + health +
+                ", maxHealth=" + maxHealth +
+                ", experience=" + experience +
+                ", attack=" + attack +
+                ", defense=" + defense;
     }
 }
--- TEMA4/POO1/mainHero.java
+++ TEMA4/POO1/mainHero.java
@@ -5,10 +5,32 @@ import java.util.Scanner;
 public class mainHero {
     private static final int defaultExperience = 0;
     private static final int defaultLvl = 1;
+
     public static void main(String[] args) {
-        Hero hero1 = hero();
+        Hero hero1 = createHero();
+        Hero hero2 = createHero();
+        int round = 0;
+        while (hero1.getHealth() > 0 && hero2.getHealth() > 0) {
+            hero2.attack(hero1);
+            hero1.attack(hero2);
+            round++;
+            System.out.println("Después de " + round + " asaltos: ");
+            System.out.println(hero1.getName()+ ": " + hero1);
+            System.out.println(hero2.getName() + ": " + hero2);
+        }
+        if (hero1.getHealth() <= 0) {
+            System.out.println(hero2.getName() + " ha ganado. " + hero1.getName() + " ha sido derrotado.");
+        } else {
+            System.out.println(hero1.getName() + " ha ganado. " + hero2.getName() + " ha sido derrotado.");
+        }
+        System.out.println(hero2.getName() + " se toma una poción para preparar su próximo asalto. " + hero1.getName() + " opta por echarse una siesta.");
+        hero2.drinkPotion();
+        hero1.rest();
+        System.out.println("Vida de " + hero2.getName() + ": " + hero2.getHealth());
+        System.out.println("Vida de " + hero1.getName() + ": " + hero1.getHealth());
     }
-    private static Hero hero(){
+
+    private static Hero createHero() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ingresa el nombre del Héroe: ");
@@ -19,7 +41,6 @@ public class mainHero {
         int attack = scanner.nextInt();
         System.out.println("Ingresa la defensa de tu Héroe: ");
         int defense = scanner.nextInt();
-
-        return new Hero(name, defaultLvl,maxHealth,maxHealth, defaultExperience,attack,defense);
+        return new Hero(name, defaultLvl, maxHealth, maxHealth, defaultExperience, attack, defense);
     }
 }
