1a506629791b6d96ac78072d95fbcb56eec604be
sectorzoom <sectorzoom@gmail.com>
2024-02-21 13:30:58 +0100

    POO4 EJ5 Añado Mage

--- TEMA4/POO4/Ej5/Mage.java
+++ TEMA4/POO4/Ej5/Mage.java
@@ -1,7 +1,8 @@
 package POO4.Ej5;
+
 import java.util.Random;
 
-public class Berserker implements ICombatiente {
+public class Mage implements ICombatiente {
     private int life;
     private int baseDamage;
     private int meleeDefense;
@@ -10,7 +11,7 @@ public class Berserker implements ICombatiente {
     private String typeOfFighter;
     static Random random = new Random();
 
-    public Berserker(int life, int baseDamage, int meleeDefense, int rangedDefense, int fighterNumber, String typeOfFighter) {
+    public Mage(int life, int baseDamage, int meleeDefense, int rangedDefense, int fighterNumber, String typeOfFighter) {
         this.life = life;
         this.baseDamage = baseDamage;
         this.meleeDefense = meleeDefense;
@@ -22,34 +23,38 @@ public class Berserker implements ICombatiente {
     @Override
     public Attack attack() {
         int failureProbability = random.nextInt(100);
-        if (failureProbability <= 20) {
+        if (failureProbability <= 10) {
             System.out.println("¡El ataque del combatiente " + typeOfFighter + " número " + fighterNumber + " ha fallado!");
             return null;
         } else {
-            int damage = baseDamage + random.nextInt(0,(100-life)/2); //Más daño cuanto menos vida tiene
+            int damage = baseDamage + random.nextInt(0,(100+life)/2); //Más daño cuanto más vida tiene
             System.out.println("¡El ataque del combatiente " + typeOfFighter + " número " + fighterNumber + " ha sido un éxito!");
-            return new Attack(damage, Attack.TypeOfDamage.PHYSICAL, Attack.TypeOfAttack.MELEE);
+            int typeOfAttackProbability = random.nextInt(100);
+            if (typeOfAttackProbability <= 10){
+                return new Attack(damage/2, Attack.TypeOfDamage.PHYSICAL, Attack.TypeOfAttack.MELEE);
+            } else {
+                return new Attack(damage, Attack.TypeOfDamage.MAGIC, Attack.TypeOfAttack.RANGED);
+            }
         }
     }
 
     @Override
     public void defend(Attack attack) {
         int randomDefense = random.nextInt(11)-5;
-        if (randomDefense < 0) {
+        if(randomDefense < 0) {
             randomDefense = 0;
         }
         int currentDefense;
-        if (attack.getTypeOfAttack() == Attack.TypeOfAttack.RANGED){
-            currentDefense = rangedDefense + randomDefense;
-        } else {
+        if (attack.getTypeOfAttack() == Attack.TypeOfAttack.MELEE) {
             currentDefense = meleeDefense + randomDefense;
+        } else  {
+            currentDefense = rangedDefense + randomDefense;
         }
         int damageReceived = attack.getDamageQuantity() - currentDefense;
-        if (damageReceived < 0) {
+        if (damageReceived < 0){
             damageReceived = 0;
         }
         life -= damageReceived;
-
     }
 
     @Override
