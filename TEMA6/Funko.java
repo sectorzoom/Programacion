a66209ecf91a7ac7c3c3da66fe2e6d2f1bd0b315
davgimrod2 <sectorzoom@gmail.com>
2024-05-28 09:55:25 +0200

    a

--- TEMA6/Funko.java
+++ TEMA6/Funko.java
@@ -1,7 +1,9 @@
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
+import lombok.Data;
 
+@Data
 public class Funko implements Serializable {
     @Serial
     private static final long serialVersionUID = 100L;
